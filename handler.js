b021556185ffee176bbb96e81bbc8bf04ff6d1ca
Marko Andersson <marko.andersson@solteq.com>
2018-08-24 10:24:55 +0300

    Fetch read books from Goodreads
    Removes ResumeService as it's not needed anymore

--- handler.js
+++ handler.js
@@ -1,9 +1,8 @@
 import resumeJson from './resume';
-import ResumeService from './src/resume-service';
+import axios from 'axios'
+import cheerio from 'cheerio'
 import middy from 'middy';
 import {cors} from 'middy/middlewares';
-
-const service = new ResumeService(resumeJson);
 
 function returnResponse(callback, response) {
     const resp = {
@@ -19,10 +18,31 @@ const resumeFunction = async (event, context, callback) => {
     returnResponse(callback, resumeJson);
 };
 
-const descriptionFunction = async (event, context, callback) => {
+const fetchBooksFromGoodreads = async (event, context, callback) => {
 
-  returnResponse(callback, service.description());
+    let rssResponse = await axios.get(
+        'https://www.goodreads.com/review/list_rss/52801025?key=2JK8I5HzRi3P-g3CiMsjDuDjDw9IueytY418EctyGg6lVhR6&shelf=read'
+    )
+
+    const $ = cheerio.load(rssResponse.data, {xmlMode: true})
+
+    const books = $('rss').find('item')
+
+    const response = []
+
+    books.each(function () {
+        const title = $(this).find('title').text()
+        const image = $(this).find('book_image_url').text()
+
+        const data = {
+            title,
+            image
+        }
+        response.push(data)
+    })
+
+    returnResponse(callback, response)
 };
 
 export const resume = middy(resumeFunction).use(cors());
-export const description = middy(descriptionFunction).use(cors());
+export const books = middy(fetchBooksFromGoodreads).use(cors());
